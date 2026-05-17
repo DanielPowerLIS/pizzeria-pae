7577bc675152083a5a953a47cb5b4c448eccb4b9
MrAdair01 <adairmartinez722@gmail.com>
2026-05-16 22:06:33 -0600

    implementación en desarrollo UsuarioDAO

--- src/pizzeria/pae/modelo/dao/UsuarioDAO.java
+++ src/pizzeria/pae/modelo/dao/UsuarioDAO.java
@@ -3,6 +3,7 @@ package pizzeria.pae.modelo.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import pizzeria.pae.modelo.MySQLConnectionManager;
 import pizzeria.pae.modelo.beans.Usuario;
 
@@ -18,8 +19,9 @@ public class UsuarioDAO {
         String apellidoPaterno = nombres[1];
         String apellidoMaterno = nombres[2];
         
-        String consulta = "SELECT * FROM pizzeriapae.usuario "
-                + "WHERE nombre = ? AND apellidoPaterno = ? AND apellidoMaterno = ?";
+        String consulta = "SELECT * " +
+                    "FROM usuario JOIN direccion ON usuario.idDireccion = direccion.idDireccion " +
+                    "WHERE nombre = ? AND apellidoPaterno = ? AND apellidoMaterno = ?";
         
         MySQLConnectionManager conexion = MySQLConnectionManager.buildConnection();
         PreparedStatement sentenciaBD = conexion.prepareStatement(consulta);
@@ -29,16 +31,132 @@ public class UsuarioDAO {
         sentenciaBD.setString(3, apellidoMaterno);
         
         ResultSet resultado = sentenciaBD.executeQuery();
+        /*
+        if(resultado.next()){
+            Usuario u = new Usuario();
+            u.idUsuario = resultado.getInt("idUsuario");
+            u.nombre = resultado.getString("nombre");
+            u.apellidoPaterno = resultado.getString("apellidoPaterno");
+            u.apellidoMaterno = resultado.getString("apellidoMaterno");
+            u.telefono = resultado.getString("telefono");
+            u.email = resultado.getString("email");
+            u.haPedido = resultado.getBoolean("haPedido");
+            u.esEmpleado = resultado.getBoolean("esEmpleado");
+            u.esActivo = resultado.getBoolean("esActivo");
+            u.nombreUsuario = resultado.getString("nombreUsuario");
+            u.contrasenia = resultado.getString("contrasenia");
+            u.direccion.idDireccion = resultado.getInt("idDireccion");
+            u.direccion.calle = resultado.getString("calle");
+            u.direccion.ciudad = resultado.getString("ciudad");
+            u.direccion.numero = resultado.getString("numero");
+            u.direccion.codigoPostal = resultado.getString("codigoPostal");
+        }
+        resultado.close();
+        conexion.close();
         
+        return u;
+        */
+        return null;
+    }
+    
+    private static String[] prepararNombre(String nombreCompleto){ 
+        String nombreSeparado[] = nombreCompleto.split(" ");
+        return nombreSeparado;
+    }
         
+    public static Usuario buscarUsuarioPorTelefono(String telefonoBuscar )throws SQLException{
+        String consulta = "SELECT * " +
+                    "FROM usuario JOIN direccion ON usuario.idUsuario = direccion.idUsuario " +
+                    "WHERE usuario.telefono = ?";
         
+        MySQLConnectionManager conexion = MySQLConnectionManager.buildConnection();
+        PreparedStatement sentenciaBD = conexion.prepareStatement(consulta);
         
+        sentenciaBD.setString(1, telefonoBuscar);
+        
+        ResultSet resultado = sentenciaBD.executeQuery();
+        /*
+        if(resultado.next()){
+            Usuario u = new Usuario();
+            u.idUsuario = resultado.getInt("idUsuario");
+            u.nombre = resultado.getString("nombre");
+            u.apellidoPaterno = resultado.getString("apellidoPaterno");
+            u.apellidoMaterno = resultado.getString("apellidoMaterno");
+            u.telefono = resultado.getString("telefono");
+            u.email = resultado.getString("email");
+            u.haPedido = resultado.getBoolean("haPedido");
+            u.esEmpleado = resultado.getBoolean("esEmpleado");
+            u.esActivo = resultado.getBoolean("esActivo");
+            u.nombreUsuario = resultado.getString("nombreUsuario");
+            u.contrasenia = resultado.getString("contrasenia");
+            u.direccion.idDireccion = resultado.getInt("idDireccion");
+            u.direccion.calle = resultado.getString("calle");
+            u.direccion.ciudad = resultado.getString("ciudad");
+            u.direccion.numero = resultado.getString("numero");
+            u.direccion.codigoPostal = resultado.getString("codigoPostal");
+        }
+        resultado.close();
+        conexion.close();
+        
+        return u;
+        */
+        return null;
+    }
+       
+    public static Usuario buscarUsuarioPorDireccion(String calle, String numero )throws SQLException{
+        String consulta = "SELECT * " +
+                    "FROM usuario JOIN direccion ON usuario.idUsuario = direccion.idUsuario " +
+                    "WHERE direccion.calle = ? AND direccion.numero = ?";
+        
+        MySQLConnectionManager conexion = MySQLConnectionManager.buildConnection();
+        PreparedStatement sentenciaBD = conexion.prepareStatement(consulta);
+        
+        sentenciaBD.setString(1, calle);
+        sentenciaBD.setString(2, numero);
+        
+        ResultSet resultado = sentenciaBD.executeQuery();
+        /*
+        if(resultado.next()){
+            Usuario u = new Usuario();
+            u.idUsuario = resultado.getInt("idUsuario");
+            u.nombre = resultado.getString("nombre");
+            u.apellidoPaterno = resultado.getString("apellidoPaterno");
+            u.apellidoMaterno = resultado.getString("apellidoMaterno");
+            u.telefono = resultado.getString("telefono");
+            u.email = resultado.getString("email");
+            u.haPedido = resultado.getBoolean("haPedido");
+            u.esEmpleado = resultado.getBoolean("esEmpleado");
+            u.esActivo = resultado.getBoolean("esActivo");
+            u.nombreUsuario = resultado.getString("nombreUsuario");
+            u.contrasenia = resultado.getString("contrasenia");
+            u.direccion.idDireccion = resultado.getInt("idDireccion");
+            u.direccion.calle = resultado.getString("calle");
+            u.direccion.ciudad = resultado.getString("ciudad");
+            u.direccion.numero = resultado.getString("numero");
+            u.direccion.codigoPostal = resultado.getString("codigoPostal");
+        }
+        resultado.close();
+        conexion.close();
+        
+        return u;
+        */
+        return null;
+    }
+    
+    public static List<Usuario> obtenerUsuarios(boolean esEmpleado){
+        if(esEmpleado){
+            List<Usuario> empleados = obtenerEmpleados();
+            return empleados;
+        }
+        List<Usuario> clientes = obtenerClientes();
+        return clientes;
+    }
+    
+    private static List<Usuario> obtenerEmpleados(){
         
     }
     
-    private static String[] prepararNombre(String nombreCompleto){
+    private static List<Usuario> obtenerClientes(){
         
-        String nombreSeparado[] = nombreCompleto.split(" ");
-        return nombreSeparado;
     }
 }
