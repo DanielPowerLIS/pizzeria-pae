fce7910c559097a0265b7f28ea4df65c517a1da2
MrAdair01 <adairmartinez722@gmail.com>
2026-05-16 19:23:33 -0600

    implementación en desarrollo UsuarioDAO

--- src/pizzeria/pae/modelo/dao/UsuarioDAO.java
+++ src/pizzeria/pae/modelo/dao/UsuarioDAO.java
@@ -1,9 +1,44 @@
 package pizzeria.pae.modelo.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import pizzeria.pae.modelo.MySQLConnectionManager;
+import pizzeria.pae.modelo.beans.Usuario;
+
 /**
  *
- * @author jdani
+ * @author adair
  */
 public class UsuarioDAO {
+    public static Usuario buscarUsuarioPorNombre(String nombreCompleto )throws SQLException{
+        String nombres[] = prepararNombre(nombreCompleto);
+        
+        String nombre = nombres[0];
+        String apellidoPaterno = nombres[1];
+        String apellidoMaterno = nombres[2];
+        
+        String consulta = "SELECT * FROM pizzeriapae.usuario "
+                + "WHERE nombre = ? AND apellidoPaterno = ? AND apellidoMaterno = ?";
+        
+        MySQLConnectionManager conexion = MySQLConnectionManager.buildConnection();
+        PreparedStatement sentenciaBD = conexion.prepareStatement(consulta);
+        
+        sentenciaBD.setString(1, nombre);
+        sentenciaBD.setString(2, apellidoPaterno);
+        sentenciaBD.setString(3, apellidoMaterno);
+        
+        ResultSet resultado = sentenciaBD.executeQuery();
+        
+        
+        
+        
+        
+    }
     
+    private static String[] prepararNombre(String nombreCompleto){
+        
+        String nombreSeparado[] = nombreCompleto.split(" ");
+        return nombreSeparado;
+    }
 }
