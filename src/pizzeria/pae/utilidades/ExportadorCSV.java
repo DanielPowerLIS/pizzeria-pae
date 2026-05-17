09930259dc14aa6f2043e8007438b0770a9877ba
DanielPowerLIS <jdaniel070905@gmail.com>
2026-05-17 11:46:31 -0600

    Clase para poder exportar en formato CSV, OJO: Pendiente de implementar el de PDF

---
+++ src/pizzeria/pae/utilidades/ExportadorCSV.java
@@ -0,0 +1,53 @@
+package pizzeria.pae.utilidades;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import pizzeria.pae.modelo.beans.DetallePedido;
+import pizzeria.pae.modelo.beans.Pedido;
+
+/**
+ *
+ * @author jdani
+ */
+public class ExportadorCSV extends Exportador<Pedido>{
+
+    private PrintWriter writer;
+    
+    @Override
+    protected void abrirDocumento(String ruta) {
+        
+        try {
+            writer = new PrintWriter(ruta);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void escribirCabecera() {
+        writer.println("Nombre del cliente,Item,Cantidad,Subtotal,Total");
+        
+    }
+
+    @Override
+    protected void escribirDatos(List<Pedido> pedidos) {
+        for(Pedido p: pedidos) {
+            for(DetallePedido dp : p.getDetallePedido()) {
+                writer.println(
+                        p.getCliente().getNombre() + "," +
+                        dp.getProducto().getNombreProducto() + "," +
+                        dp.getCantidad() + "," +
+                        dp.getSubtotal() + "," +
+                        p.getTotal()
+                );
+            }
+        }
+    }
+
+    @Override
+    protected void cerrarDocumento() {
+        writer.close();
+    }
+    
+}
