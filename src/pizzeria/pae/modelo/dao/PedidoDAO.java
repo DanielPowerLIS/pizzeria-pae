90622fc9e56a607e24b910e82edc1237fcbc7205
MrAdair01 <adairmartinez722@gmail.com>
2026-05-18 21:56:41 -0600

    PedidoDAO implementación

--- src/pizzeria/pae/modelo/dao/PedidoDAO.java
+++ src/pizzeria/pae/modelo/dao/PedidoDAO.java
@@ -132,4 +132,83 @@ public class PedidoDAO {
         
         return detalles;
     }
+    
+    public static List<Pedido> buscarPedidoPorEstado(String estado)throws SQLException{
+        String consulta = "SELECT " +
+                        "p.idPedido, p.codigo AS codigoPedido, p.fecha, p.estado, p.totalAPagar, " +
+                        "u.idUsuario, u.nombre, u.apellidoPaterno, u.apellidoMaterno, u.telefono, " +
+                        "u.email, u.haPedido, u.esEmpleado, u.esActivo, u.nombreUsuario, u.contrasenia, " +
+                        "d.idDireccion, d.calle, d.ciudad, d.numero, d.codigoPostal " +
+                        "FROM pedido p " +
+                        "INNER JOIN usuario u ON p.idUsuario = u.idUsuario " +
+                        "INNER JOIN direccion d ON u.idUsuario = d.idUsuario " +
+                        "WHERE p.estado = ?";
+        
+        MySQLConnectionManager conexion = MySQLConnectionManager.buildConnection();
+        PreparedStatement sentenciaBD = conexion.prepareStatement(consulta);
+        
+        sentenciaBD.setString(1, estado);
+        
+        ResultSet resultado = sentenciaBD.executeQuery();
+        
+        List<Pedido> pedidos= null;
+        
+        if(resultado != null){
+            pedidos = new ArrayList<>();
+            while(resultado.next()){
+                Pedido p = new Pedido();
+                p.setIdPedido(resultado.getInt("idPedido"));
+                p.setFecha(resultado.getDate("fecha").toLocalDate());
+                p.setEstado(resultado.getString("estado"));
+                p.setTotal(resultado.getBigDecimal("totalAPagar"));
+
+                Direccion d = new Direccion();
+                d.setIdDireccion(resultado.getInt("idDireccion"));
+                d.setCalle(resultado.getString("calle"));
+                d.setCiudad(resultado.getString("ciudad"));
+                d.setNumero(resultado.getString("numero"));
+                d.setCodigoPostal(resultado.getString("codigoPostal"));
+
+                Usuario u = new Usuario();
+                u.setIdUsuario(resultado.getInt("idUsuario"));
+                u.setNombre(resultado.getString("nombre"));
+                u.setApellidoPaterno(resultado.getString("apellidoPaterno"));
+                u.setApellidoMaterno(resultado.getString("apellidoMaterno"));
+                u.setTelefono(resultado.getString("telefono"));
+                u.setEmail(resultado.getString("email"));
+                u.setHaPedido(resultado.getBoolean("haPedido"));
+                u.setEsEmpleado(resultado.getBoolean("esEmpleado"));
+                u.setEsActivo(resultado.getBoolean("esActivo"));
+                u.setNombreUsuario(resultado.getString("nombreUsuario"));
+                u.setContrasenia(resultado.getString("contrasenia"));
+                
+                u.setDireccion(d);
+                p.setCliente(u);
+                
+                p.setDetallePedido(obtenerDetalles(p.getIdPedido()));
+                
+                pedidos.add(p);
+                    
+            }
+        }
+        
+        resultado.close();
+        conexion.close();
+        
+        return pedidos;
+    }
+    
+    public static List<Pedido> buscarPedidosPorUsuario(){
+        String consulta = "SELECT " +
+                        "p.idPedido, p.codigo AS codigoPedido, p.fecha, p.estado, p.totalAPagar, " +
+                        "u.idUsuario, u.nombre, u.apellidoPaterno, u.apellidoMaterno, u.telefono, " +
+                        "u.email, u.haPedido, u.esEmpleado, u.esActivo, u.nombreUsuario, u.contrasenia, " +
+                        "d.idDireccion, d.calle, d.ciudad, d.numero, d.codigoPostal " +
+                        "FROM pedido p " +
+                        "INNER JOIN usuario u ON p.idUsuario = u.idUsuario " +
+                        "INNER JOIN direccion d ON u.idUsuario = d.idUsuario " +
+                        "WHERE p.estado = ?";
+        
+        return null;
+    }
 }
--- src/pizzeria/pae/modelo/dao/UsuarioDAO.java
+++ src/pizzeria/pae/modelo/dao/UsuarioDAO.java
@@ -68,8 +68,20 @@ public class UsuarioDAO {
     }
     
     private static String[] prepararNombre(String nombreCompleto){ 
-        String nombreSeparado[] = nombreCompleto.split(" ");
-        return nombreSeparado;
+        String nombrePartes[] = nombreCompleto.split(" ");
+        Integer partes = nombrePartes.length;
+        
+        String apellidoPaterno = nombrePartes[partes - 2];
+        String apellidoMaterno = nombrePartes[partes - 1];
+        
+        StringBuilder unirNombre = new StringBuilder();
+        for(int i = 0; i < partes - 2; i++){
+            unirNombre.append(nombrePartes[i]).append("");
+        }
+        
+        String nombre = unirNombre.toString().trim();
+        
+        return new String[]{nombre, apellidoPaterno, apellidoMaterno};
     }
         
     public static Usuario buscarUsuarioPorTelefono(String telefonoBuscar )throws SQLException{
